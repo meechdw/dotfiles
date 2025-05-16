cd015780ba26e3863b4aa0c0e3af9b65c1f3a7ef
Mitchell Wilson <mitchelldw01@gmail.com>
2025-05-15 22:11:48 -0500

    Add AWS CLI to system flake and boostrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,18 +5,33 @@ set -e
 mkdir -p ~/src
 cd ~/src
 
-git clone https://github.com/meechdw/dotfiles.git
-cd dotfiles
-
 softwareupdate --install-rosetta
 
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install)
+
+git clone https://github.com/meechdw/dotfiles.git
+cd dotfiles
+git remote set-url origin git@github.com:meechdw/dotfiles.git
 
 . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 
 nix run nix-darwin/master#darwin-rebuild --extra-experimental-features 'nix-command flakes' --impure -- switch --flake ./.config/nix#Main
 
 /run/current-system/sw/bin/doppler login
+
+ANTHROPIC_API_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain ANTHROPIC_API_KEY)
+security add-generic-password -a "$USER" -s "ENV_ANTHROPIC_API_KEY" -w "$ANTHROPIC_API_KEY"
+
+AWS_ACCESS_KEY_ID=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain AWS_SECRET_ACCESS_KEY)
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+EOF
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
@@ -30,14 +45,19 @@ cat > ~/.ssh/id_ed25519.pub <<< "$GIT_SSH_PUBLIC_KEY"
 chmod 644 ~/.ssh/id_ed25519.pub
 
 eval "$(ssh-agent -s)"
-echo -e "Host github.com\n  AddKeysToAgent yes\n  IdentityFile ~/.ssh/id_ed25519" >> ~/.ssh/config
+
+cat >> ~/.ssh/config <<EOF
+Host github.com
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_ed25519
+EOF
+
 ssh-add ~/.ssh/id_ed25519
 
 git config --global user.name "Mitchell Wilson"
 git config --global user.email "mitchelldw01@gmail.com"
 
-ANTHROPIC_API_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain ANTHROPIC_API_KEY)
-security add-generic-password -a "$USER" -s "ENV_ANTHROPIC_API_KEY" -w "$ANTHROPIC_API_KEY"
+defaults write "$(osascript -e 'id of app "Cursor"')" ApplePressAndHoldEnabled -bool false && osascript -e 'quit app "Cursor"' && open -a "Cursor"
 
 echo -e "\nBootstrap complete. Reboot your system."
 
