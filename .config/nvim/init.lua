dd2b3c09bfe79922b56d3c84597c28f6336cbcb6
Mitchell Wilson <mitchelldw01@gmail.com>
2025-05-21 22:29:30 -0500

    Remove Neovide settings from init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,15 +16,4 @@ vim.opt.cmdheight = 0
 vim.opt.laststatus = 3
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,t:block-TermCursor"
 
-vim.o.guifont = "GeistMono Nerd Font:h16"
-vim.opt.linespace = 2
-vim.g.neovide_padding_top = 0
-vim.g.neovide_padding_bottom = 0
-vim.g.neovide_padding_right = 0
-vim.g.neovide_padding_left = 0
-vim.g.neovide_show_border = false
-vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_floating_shadow = false
-
 require("config.lazy")
