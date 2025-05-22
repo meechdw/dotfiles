8e653f6b8980123ae180b28b3318fe400e5d00f8
Mitchell Wilson <mitchelldw01@gmail.com>
2025-05-21 22:25:17 -0500

    Replace oil with mini.files

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,14 +1,23 @@
 return {
-  "echasnovski/mini.icons",
-  opts = {},
-  lazy = true,
-  specs = {
-    { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+  {
+    "echasnovski/mini.icons",
+    opts = {},
+    lazy = true,
+    specs = {
+      { "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
+    },
+    init = function()
+      package.preload["nvim-web-devicons"] = function()
+        require("mini.icons").mock_nvim_web_devicons()
+        return package.loaded["nvim-web-devicons"]
+      end
+    end,
   },
-  init = function()
-    package.preload["nvim-web-devicons"] = function()
-      require("mini.icons").mock_nvim_web_devicons()
-      return package.loaded["nvim-web-devicons"]
-    end
-  end,
+  {
+    "echasnovski/mini.files",
+    opts = {},
+    keys = {
+      { "<leader>n", "<cmd>lua MiniFiles.open()<CR>" },
+    },
+  },
 }
