5458d340af6addc569dcfc75b1d706a9f84bf1cc
Mitchell Wilson <mitchelldw01@gmail.com>
2025-06-03 20:12:36 -0500

    Improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+bin="/run/current-system/sw/bin"
+
 mkdir -p ~/src
 cd ~/src
 
@@ -17,16 +19,16 @@ git remote set-url origin git@github.com:meechdw/dotfiles.git
 
 nix run nix-darwin/master#darwin-rebuild --extra-experimental-features 'nix-command flakes' --impure -- switch --flake ./.config/nix#Main
 
-/run/current-system/sw/bin/doppler login
+"$bin/doppler" login
 
-ANTHROPIC_API_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain ANTHROPIC_API_KEY)
+ANTHROPIC_API_KEY=$("$bin/doppler" secrets get --project personal --plain ANTHROPIC_API_KEY)
 security add-generic-password -a "$USER" -s "ENV_ANTHROPIC_API_KEY" -w "$ANTHROPIC_API_KEY"
 
-DEEPSEEK_API_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain DEEPSEEK_API_KEY)
+DEEPSEEK_API_KEY=$("$bin/doppler" secrets get --project personal --plain DEEPSEEK_API_KEY)
 security add-generic-password -a "$USER" -s "ENV_DEEPSEEK_API_KEY" -w "$DEEPSEEK_API_KEY"
 
-AWS_ACCESS_KEY_ID=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain AWS_ACCESS_KEY_ID)
-AWS_SECRET_ACCESS_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain AWS_SECRET_ACCESS_KEY)
+AWS_ACCESS_KEY_ID=$("$bin/doppler" secrets get --project personal --plain AWS_ACCESS_KEY_ID)
+AWS_SECRET_ACCESS_KEY=$("$bin/doppler" secrets get --project personal --plain AWS_SECRET_ACCESS_KEY)
 
 mkdir -p ~/.aws
 
@@ -46,11 +48,11 @@ mv session-manager-plugin.pkg ~/.local/bin/
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-GIT_SSH_PRIVATE_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain GIT_SSH_PRIVATE_KEY)
+GIT_SSH_PRIVATE_KEY=$("$bin/doppler" secrets get --project personal --plain GIT_SSH_PRIVATE_KEY)
 cat > ~/.ssh/id_ed25519 <<< "$GIT_SSH_PRIVATE_KEY"
 chmod 600 ~/.ssh/id_ed25519
 
-GIT_SSH_PUBLIC_KEY=$(/run/current-system/sw/bin/doppler secrets get --project personal --plain GIT_SSH_PUBLIC_KEY)
+GIT_SSH_PUBLIC_KEY=$("$bin/doppler" secrets get --project personal --plain GIT_SSH_PUBLIC_KEY)
 cat > ~/.ssh/id_ed25519.pub <<< "$GIT_SSH_PUBLIC_KEY"
 chmod 644 ~/.ssh/id_ed25519.pub
 
@@ -66,6 +68,8 @@ ssh-add ~/.ssh/id_ed25519
 
 git config --global user.name "Mitchell Wilson"
 git config --global user.email "mitchelldw01@gmail.com"
+
+"$bin/bat" cache --build
 
 echo -e "\nBootstrap complete. Reboot your system."
 
